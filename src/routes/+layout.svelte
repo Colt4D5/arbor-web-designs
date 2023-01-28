188555195c5f2b3680608b337dff2fcf4273596b
Colton <allen.colton@hotmail.com>
2023-01-27 23:14:13 -0800

    added picoCSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
+	import '@picocss/pico/css/pico.min.css';
 	import '../app.postcss';
 </script>
 
-<slot />
+<header class="container">
+	<nav>
+		<ul><li><strong>Title</strong></li></ul>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/">Home</a></li>
+			<li><a href="/">Home</a></li>
+			<li><a href="/">Home</a></li>
+		</ul>
+	</nav>
+</header>
+<main class="container">
+	<slot />
+</main>
+<footer class="container">Footer</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to <span>testing</span> SvelteKit</h1>
+<h1>Welcome to <span>SvelteKit</span></h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a role="button" href="/">This is a button</a>
