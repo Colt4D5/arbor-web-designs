3aa8ca3a86a8d894724128d227a59f02ae78fe56
Colton <allen.colton@hotmail.com>
2023-02-06 23:02:45 -0800

    migrated to adapter-netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,20 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
+    }),
 		alias: {
 			$components: path.resolve('./src/lib/components'),
 			$assets: path.resolve('./src/lib/assets'),
